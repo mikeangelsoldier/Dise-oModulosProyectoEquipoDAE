94de36c0cf5980e7ab1bdd6fdcca7fcd85b116cc
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2019-02-17 20:30:53 -0600

    primer diseño de vistas

---
+++ DiseñoProyectoEquipoDAE/src/Controlador/ControladorVistaMeseros.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+/**
+ *
+ * @author Mike
+ */
+public class ControladorVistaMeseros implements Initializable {
+
+    @FXML
+    private JFXTextField txtCodigoProducto;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+
+}
